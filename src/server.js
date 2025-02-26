f9e809698370fbce72b9a43bf5471d16ccd484d2
viniciuserierf <viniciuserierf@gmail.com>
2025-02-25 22:40:05 -0300

    Adicionado o cors e feito a atualização no env e vercel

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(cors({
+  origin: process.env.ULR_FRONT_END,
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
